0cbcb0b5a84e5ea1e7dda66b6e52c2fb5ebd2bbc
Tristyn <expiredrice@live.ca>
2014-11-27 23:10:39 -0800

    Init Commit Caroline because hunter named the business library goldrush >_>_>_>_>_>_>_>

---
+++ Caroline/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Caroline.Startup))]
+namespace Caroline
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
