25a1eb54b223d19b42602912024691323d07a640
Tristyn <expiredrice@live.ca>
2014-12-02 07:13:34 -0800

    Add GameFactory

---
+++ GoldRush/GameFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoldRush
+{
+    public class GameFactory
+    {
+        IGoldRushGame Create()
+        {
+            // 02/12/2014: Hunter, if you have readonly state that you want to share between games,
+            // this would be the place to inject it.
+            return new Game();
+        }
+    }
+}
