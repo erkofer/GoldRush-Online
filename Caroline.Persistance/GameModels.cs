e43d099525cf0b88cf11cc5d54db4c492e76733e
Tristyn <tristynstimpson@gmail.com>
2014-12-06 09:26:16 -0800

    Lazy loading DbContext

--- Caroline.Persistance/GameModels.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Caroline.Persistance
-{
-    public class Game
-    {
-        
-    }
-}
--- Caroline.Persistence/UnitOfWork.cs
+++ Caroline.Persistence/UnitOfWork.cs
@@ -5,16 +5,20 @@ namespace Caroline.Persistence
 {
     class UnitOfWork : IUnitOfWork
     {
-        readonly GoldRushDbContext _context = new GoldRushDbContext();
-        public IRepository<Game> Games { get; private set; }
-        public IRepository<ApplicationUser> Users { get; private set; }
-        public IRepository<IdentityRole> Roles { get; private set; }
+        GoldRushDbContext _context;
+        IRepository<IdentityRole> _roles;
+        IRepository<Game> _games;
+        IRepository<ApplicationUser> _users;
+
+        GoldRushDbContext Context => _context ?? (_context = new GoldRushDbContext());
+
+        public IRepository<IdentityRole> Roles => _roles ?? (_roles = new Repository<IdentityRole>(Context));
+        public IRepository<Game> Games => _games ?? (_games = new Repository<Game>(Context));
+        public IRepository<ApplicationUser> Users => _users ?? (_users = new Repository<ApplicationUser>(Context));
 
         public void Dispose()
         {
-            Games = new Repository<Game>(_context);
-            Users = new Repository<ApplicationUser>(_context);
-            Roles = new Repository<IdentityRole>(_context);
+            _context?.Dispose();
         }
     }
 }
