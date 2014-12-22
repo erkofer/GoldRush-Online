fd1e6d6abec1dd5b9c41501cd7539cd6ac1bbb98
Tristyn <expiredrice@live.ca>
2014-12-22 05:31:24 -0800

    Add caching of previously sent GameStates

---
+++ Caroline.App/GameStateCache.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Web;
+using System.Web.Caching;
+using Caroline.App.Models;
+using JetBrains.Annotations;
+
+namespace Caroline.App
+{
+    class GameStateCache
+    {
+        [CanBeNull]
+        public GameState GetGameData(string sessionGuid)
+        {
+            return HttpRuntime.Cache.Get("game_" + sessionGuid) as GameState;
+        }
+
+        public void SetGameData([NotNull] string sessionGuid, [NotNull] GameState state)
+        {
+            if (sessionGuid == null) throw new ArgumentNullException("sessionGuid");
+            if (state == null) throw new ArgumentNullException("state");
+            
+            HttpRuntime.Cache.Add("game_" + sessionGuid, state,
+                dependencies: null, 
+                absoluteExpiration: Cache.NoAbsoluteExpiration, 
+                slidingExpiration: TimeSpan.FromSeconds(30), 
+                priority: CacheItemPriority.Normal, 
+                onRemoveCallback: null);
+        }
+    }
+}
---
+++ Caroline.App/ProtoBufHelpers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using ProtoBuf;
+
+namespace Caroline.App
+{
+    public static class ProtoBufHelpers
+    {
+        public static string Serialize<T>(T data)
+        {
+            // we have to serialize to an expandable memorystream then copy it to a byte[]
+            // instead of serializing to a string directly
+            var stream = new MemoryStream(4096);
+            Serializer.Serialize(stream, data);
+            var message = Convert.ToBase64String(stream.GetBuffer(), 0, (int)stream.Position);
+            return message;
+        }
+
+        public static T Deserialize<T>(string data)
+        {
+            var bytes = new byte[data.Length * sizeof(char)];
+            Buffer.BlockCopy(data.ToCharArray(), 0, bytes, 0, bytes.Length);
+            var stream = new MemoryStream(bytes, false);
+            return Serializer.Deserialize<T>(stream);
+        }
+    }
+}
