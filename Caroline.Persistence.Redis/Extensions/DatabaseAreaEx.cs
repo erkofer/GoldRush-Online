316caf6885dd63c349042a1e08cf2dccc2cbd2d7
Tristyn <tristynstimpson@gmail.com>
2015-04-01 01:36:18 -0700

    Add DatabaseArea extension methods for creating redis lists

--- Caroline.Persistence.Redis/Extensions/DatabaseAreaEx.cs
+++ Caroline.Persistence.Redis/Extensions/DatabaseAreaEx.cs
@@ -5,11 +5,6 @@ namespace Caroline.Persistence.Redis.Extensions
 {
     public static class DatabaseAreaEx
     {
-        public static IDatabaseArea CreateSubArea(this IDatabaseArea area, RedisKey id)
-        {
-            return area.CreateSubArea(id);
-        }
-
         public static IAutoKeyEntityTable<TEntity> Set<TEntity>(this IDatabaseArea area, RedisKey id, ILongTable idIncrementDb, ISerializer<TEntity> serializer, IIdentifier<TEntity, long> identifier, TimeSpan? defaultExpiry = null)
         {
             var db = area.CreateSubArea(id);
@@ -64,6 +59,23 @@ namespace Caroline.Persistence.Redis.Extensions
         {
             var db = area.CreateSubArea(id);
             return new RedisStringTable(db, defaultExpiry);
+        }
+
+        public static IEntityListTable<TEntity> SetList<TEntity>(this IDatabaseArea area, RedisKey id,
+            ISerializer<TEntity> serializer, IIdentifier<TEntity, RedisKey> identifier)
+        {
+            // there is no defaultExpiry, as redis creates lists randomly during operations such as LPUSH,
+            // and we have to check key existence before every type of operation, then set expiry
+            // this is doable, but requires many small scripts
+            var db = area.CreateSubArea(id);
+            return new RedisEntityListTable<TEntity>(db, serializer, identifier);
+        }
+
+        public static IEntityListTable<TEntity> SetList<TEntity>(this IDatabaseArea area, RedisKey id)
+            where TEntity : IIdentifiableEntity<RedisKey>
+        {
+            // see other comment on the other method overload about defaultExpiry
+            return SetList(area, id, Objects<TEntity>.Serializer, Objects<TEntity, RedisKey>.Identifier);
         }
 
         static class Objects<TEntity>
