9f076c845eb2fb6ec120fe2072e8591fbf0a539f
Tristyn <tristynstimpson@gmail.com>
2015-04-17 17:01:15 -0700

    Add Caroline.Admin console app with del command.

---
+++ Caroline.Persistence.Models/SaveState.cs
@@ -0,0 +1,9 @@
+﻿using Caroline.Persistence.Redis;
+
+namespace Caroline.Persistence.Models
+{
+    public partial class SaveState : IIdentifiableEntity<long>
+    {
+        public long Id { get; set; }
+    }
+}
