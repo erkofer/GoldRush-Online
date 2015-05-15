c1e5975ccba81c3b49efefe6aae5369a4f3d2a97
Tristyn <tristynstimpson@gmail.com>
2015-05-14 17:46:22 -0700

    removed and ignore autogenerated .js files

--- Caroline/Scripts/_references.js
+++ Caroline/Scripts/_references.js
@@ -1,7 +1,6 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="bootstrap.js" />
 /// <reference path="bytebufferab.js" />
-/// <reference path="game.min.js" />
 /// <reference path="jquery-1.10.2.js" />
 /// <reference path="jquery.signalr-2.1.2.js" />
 /// <reference path="jquery.validate.js" />
--- Caroline/Scripts/chat.js
+++
@@ -1,7 +0,0 @@
-﻿var Chat;
-(function (Chat) {
-    function sendGlobalMessage(message) {
-    }
-    Chat.sendGlobalMessage = sendGlobalMessage;
-})(Chat || (Chat = {}));
-//# sourceMappingURL=chat.js.map
